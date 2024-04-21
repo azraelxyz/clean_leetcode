82319197c2d433db3c1ff26dd63e2fe9f0d30ecf
xwhuang <xwhuang123@gmail.com>
2024-04-21 10:02:49 +0800

    add leetcode1455.py

---
+++ leetcode1455.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+        for i, word in enumerate(words):
+            if word.startswith(searchWord):
+                return i+1
+        return -1
+
