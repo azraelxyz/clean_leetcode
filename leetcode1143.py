ecd693aa86fd49fac7667aec9dfb7f587c971de2
xwhuang <xwhuang123@gmail.com>
2024-04-29 22:32:24 +0800

    update leetcode1143.py

--- leetcode1143.py
+++ leetcode1143.py
@@ -4,7 +4,7 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-        DP = [[0 for j in range(m+2)] for i in range(n+2)]
+        DP = [[0 for j in range(m+1)] for i in range(n+1)]
         for i in range(1, n+1):
             for j in range(1, m+1):
                 if text1[i-1] == text2[j-1]:
