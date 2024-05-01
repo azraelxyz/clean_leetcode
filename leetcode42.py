1f48728565c0cba6a3d809dd09ad6accb71ac8a6
xwhuang <xwhuang123@gmail.com>
2024-05-01 19:15:08 +0800

    update leetcode42.py

--- leetcode42.py
+++ leetcode42.py
@@ -2,24 +2,28 @@
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        maxHeight = max(height)
+        maxHead = self.buildMaxHead(height)
+        maxTail = self.buildMaxTail(height)
+
         count = 0
-        _max = 0
-
-        maxHead = {}
-        for i in range(0, n):
-            _max = max(_max, height[i])
-            maxHead[i] = _max
-
-        _max = 0
-        maxTail = {}
-        for i in range(n-1, 0, -1):
-            _max = max(_max, height[i])
-            maxTail[i] = _max
-
-        for i in range(1, n-1):
+        for i in range(1, len(height)-1):
             candidate = min(maxHead[i-1], maxTail[i+1])
             count += max(candidate - height[i], 0)
         return count
+
+    def buildMaxHead(self, height):
+        _max = 0
+        maxHead = {}
+        for i in range(0, len(height)):
+            _max = max(_max, height[i])
+            maxHead[i] = _max
+        return maxHead
+    
+    def buildMaxTail(self, height):
+        _max = 0
+        maxTail = {}
+        for i in range(len(height) - 1, 0, -1):
+            _max = max(_max, height[i])
+            maxTail[i] = _max
+        return maxTail
 
