a54bf540f1fa09021988149b71fcacebc141f534
xwhuang <xwhuang123@gmail.com>
2024-05-07 22:04:09 +0800

    update leetcode1268_2.py

--- leetcode1268_2.py
+++ leetcode1268_2.py
@@ -1,24 +1,34 @@
 # https://leetcode.com/problems/search-suggestions-system/
 
 class Solution:
+    def __init__(self):
+        self.MAX_PARTIAL_RETURN = 3
+
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products = sorted(products)
         ret = []
-        MAX_PARTIAL_RETURN = 3
         for i in range(len(searchWord)):
-            partial_ret = []
-            non_prefix_indices = []
             key = searchWord[:i+1]
-            for i, product in enumerate(products):
-                if len(partial_ret) >= MAX_PARTIAL_RETURN:
-                    break
-                if product.startswith(key):
-                    partial_ret.append(product)
-                else:
-                    non_prefix_indices.append(i)
-            for i in range(len(non_prefix_indices)-1, 0, -1):
-                index = non_prefix_indices[i]
-                del products[index]
+            partial_ret, non_prefix_indices = self.linearSearch(products, key)
             ret.append(partial_ret)
+            products = self.removeNonCandidate(products, non_prefix_indices)
         return ret
+
+    def linearSearch(self, products, key):
+        non_prefix_indices = []
+        partial_ret = []
+        for i, product in enumerate(products):
+            if len(partial_ret) >= self.MAX_PARTIAL_RETURN:
+                break
+            if product.startswith(key):
+                partial_ret.append(product)
+            else:
+                non_prefix_indices.append(i)
+        return partial_ret, non_prefix_indices
+
+    def removeNonCandidate(self, products, non_prefix_indices):
+        for i in range(len(non_prefix_indices)-1, 0, -1):
+            index = non_prefix_indices[i]
+            del products[index]
+        return products
 
